8e1fa06675216e0b8a182e7855b939fdbfa88bf4
teodora96 <galateica@hotmail.com>
2019-05-04 22:04:02 +0200

    Changes in UserController and UserRepositoryImpl - this is not finished.

--- planning_vacation_app/src/main/java/com/telegroupltd/planning_vacation_app/controller/UserController.java
+++ planning_vacation_app/src/main/java/com/telegroupltd/planning_vacation_app/controller/UserController.java
@@ -1,10 +1,16 @@
 package com.telegroupltd.planning_vacation_app.controller;
 
+import com.telegroupltd.planning_vacation_app.common.exceptions.BadRequestException;
 import com.telegroupltd.planning_vacation_app.common.exceptions.ForbiddenException;
 import com.telegroupltd.planning_vacation_app.controller.genericController.GenericController;
 import com.telegroupltd.planning_vacation_app.model.User;
+//import com.telegroupltd.planning_vacation_app.repository.CompanyRepository;
 import com.telegroupltd.planning_vacation_app.repository.UserRepository;
+import com.telegroupltd.planning_vacation_app.util.Notification;
+import com.telegroupltd.planning_vacation_app.util.UserLoginInformation;
+import com.telegroupltd.planning_vacation_app.util.Util;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -22,20 +28,39 @@ import java.util.List;
 public class UserController extends GenericController<User, Integer> {
 
     private final UserRepository userRepository;
+   // private final CompanyRepository companyRepository;
 
     @PersistenceContext
     private EntityManager entityManager;
 
+
+    @Value("1")
+    private Integer superAdmin;
+    @Value("2")
+    private Integer admin;
+    @Value("3")
+    private Integer director;
+    @Value("4")
+    private Integer secretary;
+    @Value("5")
+    private Integer sectorManager;
+    @Value("6")
+    private Integer worker;
+
+    @Value("Postoji korisnik sa ovim korisnickim imenom.")
+    private String badRequestUsernameExists;
+
     @Autowired
-    public UserController(UserRepository userRepository){
-    super(userRepository); //genericki kontroler ima u sebi privatno polje JpaRepository (spring kreira upite nad bazom)
+    public UserController(UserRepository userRepository/*, CompanyRepository companyRepository*/){
+    super(userRepository);
     this.userRepository=userRepository;
+   // this.companyRepository=companyRepository;
 }
 //Override metoda: insert, update, delete, getAll(ne smije se vidjeti sifra), getById(ne smije se vidjeti sifra)
 //Implementirati metode: login, logout, ...
 
 
-    /*
+/*
 
     @Override
     @RequestMapping(method = RequestMethod.POST)
@@ -44,33 +69,59 @@ public class UserController extends GenericController<User, Integer> {
     public @ResponseBody
     User insert(@RequestBody User user) throws BadRequestException {
      ////   if(userRepository.countAllByCompanyIdAndEmail(userBean.getUser().getCompanyId(), user.getEmail()).compareTo(Integer.valueOf(0)) == 0){
-          //  if(Validator.validateEmail(user.getEmail())){
-                //String randomToken = Util.randomString(randomStringLength);
+            if(Util.validateEmail(user.getEmail())){
                 User newUser = new User();
-                newUser.setUsername(null);  //stavicemo prvi dio iz e-mail-a I dodati neki random dio ->metoda
-                newUser.setPassword(null); //generisacemo ranodm sifru  i random salt  ->metode
-                newUser.setEmail(user.getEmail());
+                String salt=Util.randomSalt();
+                String newPassword=Util.randomPassword();
+                String username=Util.generateUsername(user.getEmail());
 
-               // newUser.setFirstName(null);
-              //  newUser.setLastName(null);
+                if(user.getUserGroupId()!=superAdmin) {   //Ako nije superadmin, imace e-mail na koji ce dobiti username i password
+                        newUser.setEmail(user.getEmail());
 
-               // newUser.setPhoto(null);
+                        User userWithUsername = userRepository.getByUsernameAndCompanyId(username, user.getCompanyId());
+                        if (userWithUsername == null) {
+                            newUser.setUsername(username);  //stavicemo prvi dio iz e-mail-a I dodati neki random dio ->metoda
+                            newUser.setPassword(Util.hashPassword(salt + newPassword));  //generisacemo ranodm sifru  i random salt  ->metode
+                        } else {
+                            throw new BadRequestException(badRequestUsernameExists);  //postoji ovaj username u  ovoj kompaniji                    }
+                        }
+                }else {
+                        newUser.setEmail(null);
+                        newUser.setUsername(user.getUsername());  //stavicemo prvi dio iz e-mail-a I dodati neki random dio ->metoda
+                        newUser.setPassword(user.getPassword());
+                }
 
+                newUser.setSalt(salt);   //generisacemo random salt
+                newUser.setFirstName(null);
+                newUser.setLastName(null);
 
-                newUser.setCompanyId(user.getCompanyId());
-              //  newUser.setRoleId(user.getRoleId());
+                if(user.getUserGroupId()!=superAdmin && user.getUserGroupId()!=admin ) {  //Svi primaju mail-ove osim superadmina i admina, prvi put
+                    newUser.setPauseFlag(user.getPauseFlag());
+                    newUser.setStartDate(user.getStartDate());
+                    newUser.setReceiveMail((byte) 1);
+                }else {
+                    newUser.setPauseFlag(null);
+                    newUser.setStartDate(null);
+                    newUser.setReceiveMail((byte) 0);
+                }
+                newUser.setSectorId(user.getSectorId());
+                newUser.setPhoto(null);
+                newUser.setUserGroupId(user.getUserGroupId());
+                if(user.getUserGroupId()!=superAdmin) {
+                    newUser.setCompanyId(user.getCompanyId());
+                }else newUser.setCompanyId(null);
                 newUser.setActive((byte) 1);
-
                 if(repo.saveAndFlush(newUser) != null){
                     entityManager.refresh(newUser);
                     logCreateAction(newUser);
-                //    Notification.sendRegistrationLink(user.getEmail().trim(), randomToken);  //slacemo username,password i PIN ompanije na email adresu
-
+                    if(user.getUserGroupId()!=superAdmin) {
+                        Notification.sendLoginLink(user.getEmail().trim(), newUser.getUsername(),  newPassword , (companyRepository.getById(newUser.getCompanyId())).getPin());  //slacemo username,password i PIN kompanije na email adresu
+                         }
                     return newUser;
                 }
                 throw new BadRequestException(badRequestInsert);
-         //   }
-          //  throw new BadRequestException(badRequestValidateEmail);
+            }
+            throw new BadRequestException(badRequestValidateEmail);
         ////}
        //// throw new BadRequestException(badRequestEmailExists);
     }
@@ -79,29 +130,53 @@ public class UserController extends GenericController<User, Integer> {
     @Override
     public @ResponseBody
     List<User> getAll() {
-      //  List<User> users = cloner.deepClone(userRepository.getAllByCompanyIdAndActive(userBean.getUser().getCompanyId(), (byte)1));
+        List<User> users = cloner.deepClone(userRepository.getAllByCompanyIdAndActive(userBean.getUser().getCompanyId(), (byte)1));
         for(User user : users){
-            user.setPassword(null);
-           // user.setSalt(null);
+            user.setPassword("");
+            user.setSalt("");
         }
         return users;
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public @ResponseBody
-    User login(@RequestBody UserInformation userInformation) throws ForbiddenException {
-        User user = userRepository.login(userInformation);
+    User login(@RequestBody UserLoginInformation userLoginInformation) throws ForbiddenException {
+        User user = userRepository.login(userLoginInformation);
         if (user == null) {
             throw new ForbiddenException("Forbidden");
-        }
-        else{
-            user.setPassword(null);
-            user.setPin(null);
+        } else {
             userBean.setUser(user);
             userBean.setLoggedIn(true);
             return userBean.getUser();
         }
     }
+
+    @RequestMapping(value = "/logedInFirstTime", method = RequestMethod.POST)
+    public @ResponseBody
+    String logedInFirstTime(@RequestBody User newUser) throws BadRequestException {
+
+        User user = entityManager.find(User.class, newUser.getId());
+
+        user.setUsername(newUser.getUsername());
+        user.setPassword(Util.hashPassword(newUser.getPassword()));
+
+        user.setFirstName(newUser.getFirstName());
+        user.setLastName(newUser.getLastName());
+
+        newUser.setReceiveMail(newUser.getReceiveMail());
+        user.setPhoto(newUser.getPhoto());
+
+        user.setActive((byte) 1);
+
+        if (repo.saveAndFlush(user) != null) {
+            return "Success";
+        }
+        throw new BadRequestException(badRequestRegistration);
+    }
+
+        }
+
+
 
 */
 
--- planning_vacation_app/src/main/java/com/telegroupltd/planning_vacation_app/repository/repositoryCustom/repositoryImpl/UserRepositoryImpl.java
+++ planning_vacation_app/src/main/java/com/telegroupltd/planning_vacation_app/repository/repositoryCustom/repositoryImpl/UserRepositoryImpl.java
@@ -1,4 +1,33 @@
 package com.telegroupltd.planning_vacation_app.repository.repositoryCustom.repositoryImpl;
 
-public class UserRepositoryImpl {
+import com.telegroupltd.planning_vacation_app.model.User;
+import com.telegroupltd.planning_vacation_app.repository.repositoryCustom.UserRepositoryCustom;
+import com.telegroupltd.planning_vacation_app.util.UserLoginInformation;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+public class UserRepositoryImpl implements UserRepositoryCustom {
+
+    private static final String SQL_LOGIN = "SELECT DISTINCT u.id, u.username, u.password, u.email, u.pause_flag, u.start_date ,u.first_name, u.last_name, u.salt, u.receive_mail, u.sector_id, u.photo, u.user_group_id, u.company_id, u.active " +
+            "FROM user u JOIN company c ON IF(u.user_group_id=1, true, u.company_id=c.id) " +
+            "WHERE u.username=? AND u.password=SHA2(u.salt+?, 512) AND IF(u.user_group_id=1, u.company_id IS NULL, c.pin=?)";
+
+    //trebam SHA2()
+
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public User login(UserLoginInformation userLoginInformation) {
+        List<User> user = entityManager.createNativeQuery(SQL_LOGIN, "UserMapping").setParameter(1, userLoginInformation.getUsername()).setParameter(2, userLoginInformation.getPassword()).setParameter(3, userLoginInformation.getCompanyPin()).getResultList();
+        if(user == null || user.isEmpty()){
+            return null;
+        }
+        else{
+            return user.get(0);
+        }
+    }
 }
