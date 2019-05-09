0c88d1f1ca9a0b573a6c341cc560de8d17c9165c
ivana998 <ivanastakic998@gmail.com>
2019-05-09 15:57:34 +0200

    Implemented methods for generating salt, username, password, for validating an email and for hashing password in class Util

--- planning_vacation_app/src/main/java/com/telegroupltd/planning_vacation_app/util/Util.java
+++ planning_vacation_app/src/main/java/com/telegroupltd/planning_vacation_app/util/Util.java
@@ -1,36 +1,79 @@
 package com.telegroupltd.planning_vacation_app.util;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Util {
 
+    static SecureRandom rnd = new SecureRandom();
+
+    //Generate random salt value
     public static String randomSalt() {
-        return "";
+
+        byte[] s = new byte[16];
+        rnd.nextBytes(s);
+        String salt = Base64.getEncoder().encodeToString(s);
+        return salt;
+
     }
 
-    /*
-    Metoda generise random lozinku duzine 8 karaktera i/ili brojeva , ili izmijesano sve.
-     */
+    //Generate random password of 8 characters with letters and numbers
     public static String randomPassword() {
 
-        return "";
+        String password = "";
+        for (int i = 0; i < 4; i++) {
+            char c = (char) (rnd.nextInt(26) + 'a');
+            Integer nb = rnd.nextInt(10);
+            password += c + nb.toString();
+        }
+
+        return password;
     }
 
-    /*
-    Generise se username koji se pravi od e-maila, tako sto se regexom odvoji dio ispred @ i onda se jos dodaju 4 generisana broja i/ili karaktera ili izmijesano.
-    */
+    //Generate username by taking a substring from email till '@' character
     public static String generateUsername(String email) {
 
-        return "";
+        String addition = "";
+        for (int i = 0; i < 2; i++) {
+            char c = (char) (rnd.nextInt(26) + 'a');
+            Integer nb = rnd.nextInt(10);
+            addition += c + nb.toString();
+        }
+        String username = email.substring(0, email.indexOf('@')) + addition;
+        return username;
     }
 
-    /*
-        Metoda vraca true ako je email validna, u suprotnom false
-    */
+    //Validate email construction, return true if valid
     public static Boolean validateEmail(String email) {
-    return false;
+
+        String p = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+        Pattern pattern = Pattern.compile(p);
+        Matcher m = pattern.matcher(email);
+        if (m.find())
+            return true;
+        else
+            return false;
     }
 
 
-    public static String hashPassword(String passwordToHash) {
+    //Hash passwordToHash with given salt
+    public static String hashPasswordSalt(String passwordToHash,String salt) {
+        try {
+            byte[] saltByte = Base64.getDecoder().decode(salt);
+            KeySpec spec = new PBEKeySpec(passwordToHash.toCharArray(), saltByte, 65536, 128);
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            byte[] hashByte = factory.generateSecret(spec).getEncoded();
+            String hash = Base64.getEncoder().encodeToString(hashByte);
+            return hash;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return "";
     }
 }
