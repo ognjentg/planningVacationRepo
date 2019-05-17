0ea712be00c2ebf890f1e13c922755b9aa714a96
milicamedan <milica.medan@student.etf.unibl.org>
2019-05-17 21:49:12 +0200

    constraints and non_working_day

--- planning_vacation_app/src/main/java/com/telegroupltd/planning_vacation_app/repository/ConstraintsRepository.java
+++ planning_vacation_app/src/main/java/com/telegroupltd/planning_vacation_app/repository/ConstraintsRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ConstraintsRepository extends JpaRepository<Constraints, Integer>, HasActiveRepository<Constraints,Integer> {
 
+    Constraints getByCompanyIdAndActive(Integer companyId, byte active);
 }
--- planning_vacation_app/src/main/java/com/telegroupltd/planning_vacation_app/repository/NonWorkingDayRepository.java
+++ planning_vacation_app/src/main/java/com/telegroupltd/planning_vacation_app/repository/NonWorkingDayRepository.java
@@ -4,6 +4,9 @@ import com.telegroupltd.planning_vacation_app.model.NonWorkingDay;
 import com.telegroupltd.planning_vacation_app.repository.genericRepository.HasActiveRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NonWorkingDayRepository extends JpaRepository<NonWorkingDay, Integer>, HasActiveRepository<NonWorkingDay,Integer> {
 
+    List<NonWorkingDay> getAllByCompanyIdAndActive(Integer companyID, Byte active);
 }
