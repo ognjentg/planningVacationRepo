3ea682e48cd69c36f97a1b09fc690c085c817806
nevencvetnic <nevencvetnic@gmail.com>
2019-06-14 09:56:56 +0200

    Fix in Repository.

--- planning_vacation_app/src/main/java/com/telegroupltd/planning_vacation_app/repository/LeaveRequestTypeRepository.java
+++ planning_vacation_app/src/main/java/com/telegroupltd/planning_vacation_app/repository/LeaveRequestTypeRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface LeaveRequestTypeRepository extends JpaRepository<LeaveRequestType,Integer>, HasActiveRepository<LeaveRequestType,Integer> {
-    List<LeaveRequestType> getAll(Integer id);
+    List<LeaveRequestType> getAllById(Integer id);
 }
